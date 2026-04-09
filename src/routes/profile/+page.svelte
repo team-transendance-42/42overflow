0eb820ea81da7e7644324947dd376163ba8e6239
Diane Wang <diwang@f0r2s17.codam.nl>
2026-04-09 15:09:21 +0200

    minor update april 8 3pm

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -9,6 +9,7 @@
   let firstname = data.user?.name?.split(' ')[0] ?? '';
   let lastname = data.user?.name?.split(' ')[1] ?? '';
   let email = data.user?.email ?? '';
+  let interests = data.user?.interests ?? '';
   let intraprofile = data.profile?.login ?? '';
   let campus = data.profile?.campus ?? '';
   let previewUrl = data.user?.image ?? '';
@@ -16,10 +17,17 @@
   let success = false;
   let loading = false;
 
+  let fileInput: HTMLInputElement;
+
   function handleFileChange(e: Event) {
     const file = (e.target as HTMLInputElement).files?.[0];
     if (file) previewUrl = URL.createObjectURL(file);
   }
+
+  function removeAvatar() {
+  previewUrl = '';
+  if (fileInput) fileInput.value = ''; // reset the input
+}
 
   async function handleUpdate() {
     error = '';
@@ -57,10 +65,10 @@
     <Avatar src={previewUrl} size="80px" />
     <label class="upload-btn">
       <strong>Upload avatar</strong>
-      <input type="file" name="avatarimage" accept="image/*" on:change={handleFileChange} />
+      <input type="file" name="avatarimage" accept="image/*" bind:this={fileInput} on:change={handleFileChange} />
     </label>
     {#if previewUrl}
-      <button type="button" class="remove-btn" on:click={() => previewUrl = ''}>
+      <button type="button" class="remove-btn" on:click={removeAvatar}>
         <strong>Remove avatar</strong>
       </button>
     {/if}
@@ -72,6 +80,7 @@
   </div>
 
   <Input label="E-mail" name="email" placeholder="E-mail" bind:value={email} />
+  <Input label="Interests" name="interests" placeholder="Interests" bind:value={interests} />
   <Input label="Campus" name="campus" placeholder="Campus" bind:value={campus} />
   <Input label="Intra Profile" name="intraprofile" placeholder="Intra profile" bind:value={intraprofile} />
 
